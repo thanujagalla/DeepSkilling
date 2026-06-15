ca3559428d2d2087f402a0bbce757b680e0a103c
thanujagalla <thanujagalla2005@gmail.com>
2026-06-15 19:34:08 +0530

    Add Document Factory pattern implementation
    Implemented a Document Factory pattern to create different document types.

--- FactoryPattern/main.java
+++ FactoryPattern/main.java
@@ -1,1 +1,56 @@
+interface Document {
+    void open();
+}
 
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document");
+    }
+}
+
+class DocumentFactory {
+
+    public static Document createDocument(String type) {
+
+        switch (type.toLowerCase()) {
+            case "word":
+                return new WordDocument();
+
+            case "pdf":
+                return new PdfDocument();
+
+            case "excel":
+                return new ExcelDocument();
+
+            default:
+                throw new IllegalArgumentException("Invalid document type");
+        }
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Document word = DocumentFactory.createDocument("word");
+        word.open();
+
+        Document pdf = DocumentFactory.createDocument("pdf");
+        pdf.open();
+
+        Document excel = DocumentFactory.createDocument("excel");
+        excel.open();
+    }
+}
