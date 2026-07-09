1c5e892c3555d9614d9baeb1013ef8d20bac76ca
thanujagalla <thanujagalla2005@gmail.com>
2026-07-09 15:14:40 +0530

    Add files via upload

---
+++ 1. spring-rest-handson_week3/Spring Core – Load_Country_from_Spring_Configuration_XML/spring-learn/src/main/java/com/cognizant/spring1_learn/Country.java
@@ -0,0 +1,31 @@
+package com.cognizant.spring1_learn;
+
+public class Country {
+
+    private String code;
+    private String name;
+
+    public Country() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country [code=" + code + ", name=" + name + "]";
+    }
+}
---
+++ 1. spring-rest-handson_week3/Spring Core – Load_Country_from_Spring_Configuration_XML/spring-learn/src/main/java/com/cognizant/spring1_learn/DisplayCountry.java
@@ -0,0 +1,17 @@
+package com.cognizant.spring1_learn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DisplayCountry {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        System.out.println(country);
+    }
+}
