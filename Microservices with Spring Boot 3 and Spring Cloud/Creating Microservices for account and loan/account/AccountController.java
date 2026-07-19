c50cb811379a67fa711525eead5ec107c2cde97a
thanujagalla <thanujagalla2005@gmail.com>
2026-07-19 22:47:47 +0530

    Add files via upload

---
+++ Microservices with Spring Boot 3 and Spring Cloud/Creating Microservices for account and loan/account/AccountController.java
@@ -0,0 +1,26 @@
+package com.cognizant.account.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AccountController {
+
+
+    @GetMapping("/accounts/{number}")
+    public Map<String,Object> getAccount(
+            @PathVariable String number) {
+
+
+        Map<String,Object> account = new HashMap<>();
+
+        account.put("number", number);
+        account.put("type", "savings");
+        account.put("balance", 234343);
+
+
+        return account;
+    }
+}
