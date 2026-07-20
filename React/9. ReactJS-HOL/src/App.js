e37c15f916f4c91f73cbb9904efa8fd916473cb1
thanujagalla <thanujagalla2005@gmail.com>
2026-07-20 20:21:02 +0530

    Add files via upload

---
+++ React/9. ReactJS-HOL/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ListofPlayers from "./ListofPlayers";
+import IndianPlayers from "./IndianPlayers";
+
+function App() {
+
+    const flag = true;
+
+    if(flag){
+
+        return <ListofPlayers/>;
+
+    }
+    else{
+
+        return <IndianPlayers/>;
+
+    }
+
+}
+
+export default App;
